130b4b9fc8d3fba6f8fdab0a1dd4d745e88f4f18
Amir Pourhadi <Alex.CE1379@Gmail.com>
2021-09-06 12:31:49 +0430

    profile model updated

--- models/profile.js
+++ models/profile.js
@@ -1,18 +1,46 @@
 const { Schema, model } = require("mongoose");
 
 const ProfileSchema = new Schema({
+  // you will get access to this with user.id
   user: { type: Schema.Types.ObjectId, ref: "user" },
   company: { type: String },
   website: { type: String },
-  skills :{
-    type: [String],
-    
-  },
+  location: { type: String },
+  status: { type: String, required: true },
+  skills: { type: [String], required: true },
+  bio: { type: String },
+  githubusername: { type: String },
   experience: [
     {
-      
-    }
-  ]
+      title: { type: String, required: true },
+      company: { type: String, required: true },
+      location: { type: String },
+      from: { type: Date, required: true },
+      to: { type: Date },
+      current: { type: Boolean, default: false },
+      description: { type: String },
+    },
+  ],
+  education: [
+    {
+      school: { type: String, required: true },
+      degree: { type: String, required: true },
+      fieldofstudy: { type: String, required: true },
+      from: { type: Date, required: true },
+      to: { type: Date },
+      current: { type: Boolean, default: false },
+      description: { type: String },
+    },
+  ],
+  social: {
+    youtube: { type: String },
+    twitter: { type: String },
+    facebook: { type: String },
+    linkedin: { type: String },
+    instagram: { type: String },
+    github: { type: String },
+  },
+  date: { type: Date, default: Date.now },
 });
 
 const Profile = model("Profile", ProfileSchema);
