89e4845275acef1a68d4224eb2bc4edee203298d
Amir Pourhadi <Alex.CE1379@Gmail.com>
2021-09-07 21:47:57 +0430

    user route updated

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,13 +3,14 @@ const { Router } = require("express");
 const router = Router();
 
 /**
- * @route GET api/users
+ * @route POST api/users
  * @description Test route
  * @access public
  */
 
-router.get("/", (req, res) => {
+router.post("/", (req, res) => {
   res.send("Users Route!");
+  console.log(req.body);
 });
 
 module.exports = router;
