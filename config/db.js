f59026159e413c08904d95e7d3e511595793923a
Amir Pourhadi <Alex.CE1379@Gmail.com>
2021-09-07 19:21:36 +0430

    deprecated fixed

--- config/db.js
+++ config/db.js
@@ -8,7 +8,10 @@ const db = config.get("mongoURI");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(db);
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
   } catch (error) {
     console.error(error.message);
 
