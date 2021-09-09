11d499543f2035c8a494646d71f7ce3ba15e6ff0
Amir Pourhadi <Alex.CE1379@Gmail.com>
2021-09-09 12:42:18 +0430

    checked if users exists

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,4 +1,7 @@
 const { Router } = require("express");
+const { check, validationResult } = require("express-validator");
+const User = require("../../models/User");
+const gravatar = require("gravatar");
 
 const router = Router();
 
@@ -8,9 +11,33 @@ const router = Router();
  * @access public
  */
 
-router.post("/", (req, res) => {
-  res.send("Users Route!");
-  console.log(req.body);
-});
+router.post(
+  "/",
+  [
+    check("name", "Name is required").not().isEmpty(),
+    check("password", "Password is required, the length should be at least 6 characters").isLength({ min: 6 }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { name, email, password } = req.body;
+
+    try {
+      // Check if user exists
+      let user = await User.findOne({ email });
+      if (user) {
+        return res.status(400).json({ errors: [{ msg: "User already exists" }] });
+      }
+
+      // Get user avatar
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Server Error!");
+    }
+  }
+);
 
 module.exports = router;
