5a840cc0e6a85665e8da313a95b9041370c80dd9
Amir Pourhadi <Alex.CE1379@Gmail.com>
2021-09-12 19:27:19 +0430

    signed with jwt

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const { Router } = require("express");
 const { check, validationResult } = require("express-validator");
 const User = require("../../models/User");
 const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
+const config = require("config");
 
 const router = Router();
 
@@ -15,6 +17,7 @@ router.post(
   "/",
   [
     check("name", "Name is required").not().isEmpty(),
+    check("email", "Email is required").isEmail(),
     check("password", "Password is required, the length should be at least 6 characters").isLength({ min: 6 }),
   ],
   async (req, res) => {
@@ -33,6 +36,21 @@ router.post(
       }
 
       // Get user avatar
+      const avatar = gravatar.url(email, { s: "200", r: "pg", d: "mm" });
+
+      user = new User({ name, email, avatar, password });
+
+      // Encrypt password
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+      await user.save();
+
+      // JsonWebToken(JWT)
+      const payload = { user: { id: user.id } };
+      jwt.sign(payload, config.get("jwtSecret"), { expiresIn: 36000000 }, (err, token) => {
+        if (err) throw err;
+        res.json({ token });
+      });
     } catch (error) {
       console.error(error.message);
       res.status(500).send("Server Error!");
